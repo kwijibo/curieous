288cb41f8a1575678b2064246d6490b9dc0955ed
Keith Alexander <github@keithalexander.co.uk>
2011-10-29 17:43:10 +0100

    some changes

--- curieous.php
+++ curieous.php
@@ -18,12 +18,16 @@ class Curieous {
 
   function __destruct(){
     $this->read_curies_json();
-    file_put_contents('curies.json',json_encode($this->curies)); 
+    file_put_contents(realpath(basename(__FILE__)).'curies.json',json_encode($this->curies)); 
   }
 
   private function read_curies_json(){
-    $curies = json_decode(file_get_contents('curies.json'),1);
-    $this->curies = array_merge($curies, $this->curies);
+    $curies = json_decode(file_get_contents(__DIR__.'/curies.json'),1);
+    if(is_array($curies)){
+      $this->curies = array_merge($curies, $this->curies);
+    } else {
+      var_dump($curies);
+    }
   }
 
   function register($prefix, $uri){
@@ -38,7 +42,9 @@ class Curieous {
   }
 
   function check($curie){
-    if(isset($this->curies[$curie])) return $this->curies[$curie];
+    if(isset($this->curies[$curie])){
+      return $this->curies[$curie];
+    }
     list($prefix, $localname) = explode(':', $curie);
     $uri = $this->uri($curie);
     if($prefix=='xsd'){
@@ -50,7 +56,8 @@ class Curieous {
         throw new CurieousNonExistentUriException("{$curie} is not an xsd datatype");
       }
     }
-    $this->vocabulary_graph->read_data($uri);
+    $doc_uri = preg_replace('@#.*@','',$uri);
+    $this->vocabulary_graph->read_data($doc_uri);
     if($this->vocabulary_graph->has_triples_about($uri)){
       $this->curies[$curie] = $uri;
       return $uri;
@@ -61,15 +68,22 @@ class Curieous {
 
   function lookup($prefix){
     if(isset($this->prefixes[$prefix])) return $this->prefixes[$prefix];
-    $json = $this->http_request_factory->make('GET', self::prefix_api . $prefix . '.file.json')->execute()->body;
-    $uri = json_decode($json)->$prefix;
-    $this->prefixes[$prefix]=$uri;
-    return $uri;
+    $prefix_doc_location = self::prefix_api . $prefix . '.file.json';
+    $json_response = $this->http_request_factory->make('GET', $prefix_doc_location)->execute();
+    if($json_response->is_success()){
+      $json = $json_response->body;
+      $uri = json_decode($json)->$prefix;
+      $this->prefixes[$prefix]=$uri;
+      return $uri;
+    } else {
+      throw new CurieousHttpFailureException("{$prefix_doc_location}");
+    }
   }
 
 }
 
 class CurieousNonExistentUriException extends Exception {}
 
+class CurieousHttpFailureException extends Exception {}
 
 ?>
